007f8f1f3b5f7f47224e633625faafc8742c194e
nsubiron <nsswdev@gmail.com>
2014-09-24 18:44:26 +0200

    Smaller license header

--- suricate/build_variables.py
+++ suricate/build_variables.py
@@ -1,19 +1,9 @@
-# Sublime Suricate is licensed under the GPL license.
+# Sublime Suricate, Copyright (C) 2013 N. Subiron
 #
-# Copyright (C) 2013 N. Subiron
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you
+# are welcome to redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
 
 """Sublime Text build variables:
   * ``${file}`` The full path to the current file, e. g., C:\Files\Chapter1.txt.

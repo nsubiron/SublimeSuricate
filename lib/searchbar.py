d15bb7b3e2d971875e77de154347536b2b530450
nsubiron <nsswdev@gmail.com>
2014-09-24 19:17:32 +0200

    Add google search

--- lib/searchbar.py
+++ lib/searchbar.py
@@ -10,24 +10,38 @@
 import sublime
 
 from suricate import Settings
+from suricate import import_module
+
+sublime_wrapper = import_module('lib.sublime_wrapper')
 
 Engines = {
-  'DuckDuckGo': 'https://duckduckgo.com/?q='
+  'DuckDuckGo': 'https://duckduckgo.com/?q=',
+  'Google': 'https://www.google.com/search?q='
 }
 
-def show(caption=None, engine=None, default='DuckDuckGo'):
+def show(caption=None, engine=None):
+    engine = _get_default_engine_if_not_valid(engine)
+    prefix = Engines[engine]
+    if not caption:
+      caption = '%s:' % engine
+    window = sublime.active_window()
+    on_done = lambda string: _on_done(prefix, string, window)
+    window.show_input_panel(caption, '', on_done, None, None)
+
+def search_selection(view=None, engine=None):
+    prefix = Engines[_get_default_engine_if_not_valid(engine)]
+    window = sublime.active_window()
+    for string in sublime_wrapper.get_selection(view):
+      _on_done(prefix, string, window)
+
+def _on_done(prefix, string, window):
+    url = prefix + '+'.join(string.split())
+    window.run_command('open_url', {'url': url})
+
+def _get_default_engine_if_not_valid(engine, default='DuckDuckGo'):
     if engine is None:
       engine = Settings.get('default_search_engine', default)
     if engine not in Engines:
       print('Search engine "%s" not implemented.' % engine)
       engine = default
-    prefix = Engines[engine]
-    if not caption:
-      caption = '%s:' % engine
-    window=sublime.active_window()
-    on_done = lambda string: _on_done(prefix, string, window)
-    window.show_input_panel(caption, '', on_done, None, None)
-
-def _on_done(prefix, string, window):
-    url = prefix + '+'.join(string.split())
-    window.run_command('open_url', {'url': url})
+    return engine

36824d5d6257aa872379bff763276e2f6e8f76c4
nsubiron <nsswdev@gmail.com>
2015-02-10 17:40:02 +0100

    Add Quick Switch Dictionary command

---
+++ lib/dictionaries.py
@@ -0,0 +1,28 @@
+# Sublime Suricate, Copyright (C) 2013 N. Subiron
+#
+# This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you
+# are welcome to redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+
+import sublime
+
+import os
+
+from suricate import Settings
+
+def switch_language(view):
+    dicts = Settings.get('quick_switch_dictionary_list', [])
+    if not dicts:
+      dicts = sublime.find_resources('*.dic')
+      if not dicts:
+        print('ERROR: no dictionary found')
+        return
+    current = view.settings().get('dictionary')
+    next_item_index = dicts.index(current) + 1 if current in dicts else 0
+    next_item = dicts[next_item_index % len(dicts)]
+    if next_item in sublime.find_resources('*.dic'):
+      view.settings().set('dictionary', next_item)
+      sublime.status_message('Dictionary changed to %s' % os.path.basename(next_item))
+    else:
+      sublime.error_message('Dictionary "%s" not available' % next_item)
